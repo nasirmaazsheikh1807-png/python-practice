ea04a51ef22113c50747e222112adc35026f33ef
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-07-07 12:30:41 +0530

    Added Prime Number Checker

---
+++ PrimeNumber_Checker.py
@@ -0,0 +1,11 @@
+print("Prime Number Checker")
+x = int(input("Enter a number: "))
+print("You entered:", x)
+if x > 1:
+    for i in range(2,x):
+        if (x % i) == 0:
+            print(x, "is not a prime number")
+            break
+        else:
+            print(x, "is a prime number")
+            break
