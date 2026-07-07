a6438a78a5259d22df18ee8c997930b4c996a146
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-07-07 13:07:14 +0530

    Cleared a Bug in Prime Number Checker

--- PrimeNumber_Checker.py
+++ PrimeNumber_Checker.py
@@ -1,11 +1,13 @@
 print("Prime Number Checker")
+
 x = int(input("Enter a number: "))
-print("You entered:", x)
-if x > 1:
-    for i in range(2,x):
-        if (x % i) == 0:
+
+if x <= 1:
+    print(x, "is not a prime number")
+else:
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
             print(x, "is not a prime number")
             break
-        else:
-            print(x, "is a prime number")
-            break
+    else:
+        print(x, "is a prime number")
