647927ee0072e3a90086b5ab6f963657ff96b57f
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-07-14 22:49:28 +0530

    Added Average Of All The Numbers Of The List

---
+++ Average_list.py
@@ -0,0 +1,11 @@
+
+print("Average of all the numbers of the list")
+numbers= [10,45,23,89,67,34]
+sum = 0
+averge = 0
+count = 0
+for i in numbers:
+    sum += i
+    count += 1
+average = sum / count
+print(average)
