82a1680b9177fd47bb93fa6ed48f04041d068ee8
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-07-23 00:13:29 +0530

    Added proper code of remove extra space from string

--- Space_corrector.py
+++ Space_corrector.py
@@ -1,14 +1,13 @@
 print("Remove Extra Spaces From String")
 n = str(input("Enter A String: "))
 new = ("")
-prev = new[-1]
-space = (" ")
 for i in n:
-    if i != "":
+    if i != " ":
         new += i
-    if prev != space:
-        new += prev
-    elif prev == space:
-        prev = i
+    else:
+        if new != "" and new[-1] != " ":
+            new += " "
+if new != "" and new[-1] == " ":
+    new = new[:-1]
 
 print(new)
