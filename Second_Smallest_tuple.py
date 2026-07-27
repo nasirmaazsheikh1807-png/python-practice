60ecdca9a33e6a919de60d1f8e13299975439db8
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-07-27 22:44:41 +0530

    One Change Done

--- Second_Smallest_tuple.py
+++ Second_Smallest_tuple.py
@@ -4,10 +4,10 @@ second_smallest = n[0]
 smallest = n[1]
 for i in n:
     if i < smallest:
-        i = smallest
-        smallest = second_smallest
+        second_smallest = smallest
+        smallest = i
 
     else:
-        if i < second_smallest:
+        if i < second_smallest and i != smallest:
             second_smallest = i
 print(second_smallest)
