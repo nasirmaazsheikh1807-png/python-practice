22e73d2add5de6b33c14bc8ca52f25927501e258
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-08-02 22:52:22 +0530

    Handled Equal Elements In The Tuple

--- Merge_sorted_tuple.py
+++ Merge_sorted_tuple.py
@@ -1,5 +1,5 @@
 print("Merge Two Sorted Tuples")
-a = (1,3,5,7,9)
+a = (1,3,5,)
 b = (2,4,6,8,10)
 new = ()
 i = 0
@@ -11,4 +11,8 @@ while i < len(a) and j < len(b):
     elif a[i] > b[j]:
         new += b[j],
         j += 1
+    elif a[i] == b[j]:
+        new += a[i],
+        j += 1
+        i += 1
 print(new)
