184c7e522331c93640676b1d710bd22c592bcfdd
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-08-14 23:37:17 +0530

    Day 41: Practice random and datetime

--- Modules.py
+++ Modules.py
@@ -19,11 +19,11 @@ print("Modules")
 # print("You Guessed The Correct Number! ")
 
 # Random Food Selector.
-import random
-foods = ["Pizza", "Burger", "Momos","Biryani","Pasta"]
+# import random
+# foods = ["Pizza", "Burger", "Momos","Biryani","Pasta"]
 # choice = random.choice(foods)
 
-print("Today's Food: ", random.sample(foods,1))
+# print("Today's Food: ", random.sample(foods,1))
 
 
 # Rndomly Shuffling A List.
@@ -31,3 +31,43 @@ print("Today's Food: ", random.sample(foods,1))
 # n = [1,2,3,4,5]
 # random.shuffle(n)
 # print(n)
+
+
+# import datetime
+# now = datetime.datetime.now()
+# print(now)
+# print(now.date())
+# print(now.time())
+# print(now.year,"-",now.month,"-",now.day)
+# print(now.strftime("%d-%m-%Y"))
+# print(now.strftime("%H:%M:%S"))
+# from datetime import datetime , timedelta
+# now = datetime.now()
+# future = now + timedelta(days=7)
+# print(future)
+# past = now - timedelta(days=7)
+# print(past)
+# Difference In Date 
+# date1 = datetime(2026,8,14)
+# date2 = datetime(2026,9,1)
+# difference = date2 - date1
+# print(difference)
+# print(difference.days)
+
+# class date.
+# from datetime import date
+# today = date.today()
+# print(today)
+# birthday = date(2005,7,18)
+# print(today - birthday)
+
+
+
+
+# Class Time.
+from datetime import time , date , datetime
+t = time(14,30,45)
+d = date.today()
+dt = datetime.combine(d,t)
+print(dt)
+# print(t)
