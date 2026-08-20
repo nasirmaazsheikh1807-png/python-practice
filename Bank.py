ab8506afeac4e07019482356db59eb1c4b9e35da
Nasirmaaz Sheikh <nasirmaazsheikh1807@gmail.com>
2026-08-20 23:45:19 +0530

    Day 48: Expanded bank system with account managmement and OOP Concepts

--- Bank.py
+++ Bank.py
@@ -3,6 +3,8 @@ class BankAccount:
     def __init__(self,owner,balance,):
         self.owner = owner
         self.__balance = balance
+    def get_balance(self):
+        return self.__balance
     def deposit(self,amount):
         self.__balance += amount
         print(f"Added ${amount} to {self.owner}'s Account")
@@ -32,7 +34,7 @@ class SavingsAccount(BankAccount):
         self.add_interest()
         self.show_balance()
     def withdraw(self, amount):
-        if self.__balance - amount < 1000:
+        if self.get_balance() - amount < 1000:
             print("Minimum Balance Required!")
         else:
             return super().withdraw(amount)
@@ -47,7 +49,33 @@ class CurrentAccount(BankAccount):
             print(f"${amount} Withdrawed From Current Account of{self.owner}. Remaining Balance is {self.__balance}")
         else:
             print("Limit Exceeded Than -5000!")        
-account1 = CurrentAccount("Rahul",5000)
-account2 = SavingsAccount("Vijay",2000,5)
-account1.withdraw(1000)
-account2.withdraw(1000)
+class Bank:
+    def __init__(self):
+        self.accounts = []
+    def add_account(self,account):
+        self.accounts.append(account)
+    def show_all_accounts(self):
+        for account in self.accounts:
+            account.show_balance()
+    def find_account(self,owner):
+        for account in self.accounts:
+            if account.owner == owner:
+                return account
+        print("Account Not Found")
+    def deposit(self,owner,amount):
+        account =self.find_account(owner)
+        if account:
+            account.deposit(amount)
+    def withdraw(self,owner,amount):
+            account =self.find_account(owner)
+            if account:
+                account.withdraw(amount)
+    
+account1 = SavingsAccount("Rahul",5000,5)
+account2 = CurrentAccount("Aman",3000)
+bank = Bank()
+bank.add_account(account1)
+bank.add_account(account2)
+
+bank.deposit("Rahul",2000)
+bank.withdraw("Rahul",1000)
